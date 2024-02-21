4348e7517984a2545f40bc50b337002bebc9a617
mike0312 <47568828+mike0312@users.noreply.github.com>
2024-02-21 13:53:05 -0600

    update2

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name = "blog"
+  name = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
