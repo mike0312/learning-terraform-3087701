f0248b9aaad527573a814c99c246fbe541623210
mike0312 <47568828+mike0312@users.noreply.github.com>
2024-02-21 11:21:53 -0600

    testing

--- main.tf
+++ main.tf
@@ -15,10 +15,15 @@ data "aws_ami" "app_ami" {
 }
 
 resource "aws_instance" "web" {
+  count = 2
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   tags = {
     Name = "HelloWorld"
+  }
+  lifcycle {
+    create_before_destroy = true
+  
   }
 }
