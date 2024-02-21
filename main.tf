2260bed608c6fdbec6924c44bc0f13a69626a576
mike0312 <47568828+mike0312@users.noreply.github.com>
2024-02-21 12:08:55 -0600

    updates

--- main.tf
+++ main.tf
@@ -17,11 +17,12 @@ data "aws_ami" "app_ami" {
 data "aws_vpc" "default" { 
   default = true
 }
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_id = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "Learning Terraform"
@@ -39,17 +40,17 @@ resource "aws_instance" "blog" {
    type        = "ingress"
    from_port   = 80
    to_port     = 80
-   portocol    = "tcp"
+   protocol    = "tcp"
    cidr_blocks = ["0.0.0.0/0"]
 
    security_group_id = aws_security_group.blog.id
  }
 
-resource "aws_security_group_rule" "blog_https_in" {
+ resource "aws_security_group_rule" "blog_https_in" {
    type        = "ingress"
    from_port   = 443
    to_port     = 443
-   portocol    = "tcp"
+   protocol    = "tcp"
    cidr_blocks = ["0.0.0.0/0"]
 
    security_group_id = aws_security_group.blog.id
@@ -59,7 +60,7 @@ resource "aws_security_group_rule" "blog_https_in" {
    type        = "egress"
    from_port   = 0
    to_port     = 0
-   portocol    = "-1"
+   protocol    = "-1"
    cidr_blocks = ["0.0.0.0/0"]
 
    security_group_id = aws_security_group.blog.id
